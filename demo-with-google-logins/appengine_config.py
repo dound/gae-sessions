21bac0aa02c68c8ed7438b9b45eae76f301a8afc
David Underhill <dgu@cs.stanford.edu>
2010-05-19 21:16:37 -0400

    update demos

--- demo-with-google-logins/appengine_config.py
+++ demo-with-google-logins/appengine_config.py
@@ -1,7 +1,11 @@
 from gaesessions import SessionMiddleware
 
+# suggestion: generate your own random key using os.urandom(64)
+import os
+COOKIE_KEY = 'do not use this key'
+
 def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
-  app = SessionMiddleware(app)
+  app = SessionMiddleware(app, cookie_key=COOKIE_KEY)
   app = recording.appstats_wsgi_middleware(app)
   return app
