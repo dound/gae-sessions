2d119531f20e745d5ae341b30cbcceb98ea110b9
David Underhill <dgu@cs.stanford.edu>
2010-05-20 15:31:00 -0400

    test framework: create test generator which runs a set of tests on several configurations of the library (memcache only, no cookies, etc.)

--- tests/test_gaesessions.py
+++ tests/test_gaesessions.py
@@ -1,7 +1,28 @@
+import logging
 from SessionTester import SessionTester
 
-def test_simple():
-    st = SessionTester(cookie_only_threshold=10*1024)
+logger = logging.getLogger('TESTS ')
+logger.setLevel(logging.DEBUG)
+
+def test_sessions():
+    for no_datastore in (False, True):
+        for cot in (0, 10*1024, 2**30):
+            if no_datastore:
+                test_db = 'without'
+            else:
+                test_db = 'with'
+            if cot == 0:
+                test_cookie = 'no data stored in cookies'
+            elif cot == 2**30:
+                test_cookie = 'data only stored in cookies'
+            else:
+                test_cookie = 'store data in cookies when its encoded size<=%dB' % cot
+            logger.info('*'*50)
+            logger.info('Testing %s datastore and %s' % (test_db, test_cookie))
+            yield check_sessions, no_datastore, cot
+
+def check_sessions(no_datastore, cookie_only_threshold):
+    st = SessionTester(no_datastore=no_datastore, cookie_only_threshold=cookie_only_threshold)
 
     st.start_request()
     st['x'] = 7
