6ea3cac6e46ed8526737bca53efd00d6e0691697
darktable <calvin@darktable.com>
2011-07-02 23:36:22 -0700

    added set_current_session for situations where you manually create a session object and want it to be auto-managed by the middleware

--- gaesessions/__init__.py
+++ gaesessions/__init__.py
@@ -34,6 +34,19 @@ def get_current_session():
     """Returns the session associated with the current request."""
     return _current_session
 
+def set_current_session(session=None, **kwds):
+    """Sets the _current_session global so the session is automanaged."""
+    global _current_session
+
+    if not session:
+        _current_session = Session(**kwds)
+    elif isinstance(session, basestring):
+        _current_session = Session(sid=session, **kwds)
+    elif isinstance(session, Session):
+        _current_session = session
+
+    return _current_session
+
 def is_gaesessions_key(k):
     return k.startswith(COOKIE_NAME_PREFIX)
 
